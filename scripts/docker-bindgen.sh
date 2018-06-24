f600dcbf5af90d5aecd015960ca25ebc234f9195
Jonas Fonseca <jonas.fonseca@gmail.com>
2018-06-23 21:40:24 -0400

    Build and test the bindgen docker image

---
+++ scripts/docker-bindgen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Wrapper script for running the dockerized scala-native-bindgen binary.
+
+set -euo pipefail
+IFS=$'\n\t'
+
+volumes=(
+  "--volume=$(pwd):/src"
+  "--volume=/usr/include:/usr/include"
+)
+
+if [[ "$#" -gt 1 ]] && [[ "$1" == /* ]]; then
+  volumes+="--volume=$1:$1"  
+fi
+
+exec docker run --rm "${volumes[@]}" scalabindgen/scala-native-bindgen "$@"
