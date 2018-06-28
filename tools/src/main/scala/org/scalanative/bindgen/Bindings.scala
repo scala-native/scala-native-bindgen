cbc1fc07ce2a03b1b2ff4592020897db9ba76c65
kornilova-l <kornilova203@gmail.com>
2018-06-28 19:21:03 +0400

    Use trait class for API

--- tools/src/main/scala/org/scalanative/bindgen/Bindings.scala
+++ tools/src/main/scala/org/scalanative/bindgen/Bindings.scala
@@ -3,12 +3,10 @@ package org.scalanative.bindgen
 import java.io.{File, PrintWriter}
 
 class Bindings(private val bindings: String) {
-  def writeToFile(pathToFile: String): Unit = {
-    val pw = new PrintWriter(new File(pathToFile))
-    try {
-      pw.write(bindings)
-    } finally {
-      pw.close()
+  def writeToFile(file: File): Unit = {
+    new PrintWriter(file) {
+      write(bindings)
+      close()
     }
   }
 }
--- tools/src/main/scala/org/scalanative/bindgen/Builder.scala
+++ tools/src/main/scala/org/scalanative/bindgen/Builder.scala
@@ -1,73 +1,111 @@
 package org.scalanative.bindgen
 
+import java.io.File
+
 import scala.collection.mutable
 
-class Builder {
-  private var library: String                     = _
-  private var pathToHeader: String                = _
-  private var packageName: String                 = _
-  private var excludePrefix: String               = _
-  private var extraArg: mutable.Seq[String]       = mutable.Seq()
-  private var extraArgBefore: mutable.Seq[String] = mutable.Seq()
+sealed trait Builder {
 
   /**
    * Set header file for which bindings will be generated
    */
-  def header(pathToHeader: String): Unit = {
-    this.pathToHeader = pathToHeader
-  }
+  def header(header: File): Unit
 
   /**
    * Library to link with, e.g. -luv for libuv
    */
-  def link(library: String): Unit = {
-    this.library = library
-  }
+  def link(library: String): Unit
 
   /**
    * Package name of generated Scala file
    */
-  def packageName(packageName: String): Unit = {
-    this.packageName = packageName
-  }
+  def packageName(packageName: String): Unit
 
   /**
    * Declarations will be removed if their names
    * contain given prefix
    */
-  def excludePrefix(prefix: String): Unit = {
-    if (!prefix.isEmpty) {
-      excludePrefix = prefix
-    }
-  }
+  def excludePrefix(prefix: String): Unit
 
   /**
    * Additional argument to append to the compiler command line
    */
-  def extraArg(arg: String): Unit = {
-    if (!arg.isEmpty) {
-      extraArg += arg
-    }
-  }
+  def extraArg(arg: String): Unit
 
   /**
    * Additional argument to append to the compiler command line
    */
-  def extraArgBefore(arg: String): Unit = {
-    if (!arg.isEmpty) {
-      extraArgBefore += arg
-    }
-  }
+  def extraArgBefore(arg: String): Unit
 
   /**
    * Run binding generator
    */
-  def generate(): Bindings = {
-    // TODO: generate bindings
-    new Bindings("")
-  }
+  def generate(): Bindings
 }
 
 object Builder {
-  def apply(): Builder = new Builder()
+  def apply(): Builder = Impl()
+
+  private final case class Impl() extends Builder {
+    private var library: String                     = _
+    private var header: File                        = _
+    private var packageName: String                 = _
+    private var excludePrefix: String               = _
+    private var extraArg: mutable.Seq[String]       = mutable.Seq()
+    private var extraArgBefore: mutable.Seq[String] = mutable.Seq()
+
+    /**
+     * Set header file for which bindings will be generated
+     */
+    def header(header: File): Unit = {
+      this.header = header
+    }
+
+    /**
+     * Library to link with, e.g. -luv for libuv
+     */
+    def link(library: String): Unit = {
+      this.library = library
+    }
+
+    /**
+     * Package name of generated Scala file
+     */
+    def packageName(packageName: String): Unit = {
+      this.packageName = packageName
+    }
+
+    /**
+     * Declarations will be removed if their names
+     * contain given prefix
+     */
+    def excludePrefix(prefix: String): Unit = {
+      require(!prefix.isEmpty)
+      excludePrefix = prefix
+    }
+
+    /**
+     * Additional argument to append to the compiler command line
+     */
+    def extraArg(arg: String): Unit = {
+      require(!arg.isEmpty)
+      extraArg :+ arg
+    }
+
+    /**
+     * Additional argument to append to the compiler command line
+     */
+    def extraArgBefore(arg: String): Unit = {
+      require(!arg.isEmpty)
+      extraArgBefore :+ arg
+    }
+
+    /**
+     * Run binding generator
+     */
+    def generate(): Bindings = {
+      // TODO: generate bindings
+      new Bindings("")
+    }
+  }
 }
