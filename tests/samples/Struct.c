46d603ef9f7c2208ca374649272f15de7f6f34de
kornilova-l <kornilova203@gmail.com>
2018-07-04 13:48:57 +0300

    Check if type is alias for struct/union before generating setter/getter

--- tests/samples/Struct.c
+++ tests/samples/Struct.c
@@ -1,7 +1,27 @@
 #include "Struct.h"
 #include <stdlib.h>
 
-point_s getPoint() {
+void setPoints(struct points *points, int x1, int y1, int x2, int y2) {
+    points->p1.x = x1;
+    points->p1.y = y1;
+    points->p2.x = x2;
+    points->p2.y = y2;
+}
+
+int getPoint(struct points *points, enum pointIndex pointIndex) {
+    switch (pointIndex) {
+    case X1:
+        return points->p1.x;
+    case X2:
+        return points->p2.x;
+    case Y1:
+        return points->p1.y;
+    case Y2:
+        return points->p2.y;
+    }
+}
+
+point_s createPoint() {
     point_s point = malloc(sizeof(struct point));
     point->x = 10;
     point->y = 20;
--- tests/samples/Struct.scala
+++ tests/samples/Struct.scala
@@ -7,16 +7,28 @@ import scala.scalanative.native._
 @native.extern
 object Struct {
   type struct_point = native.CStruct2[native.CInt, native.CInt]
+  type point = struct_point
+  type struct_points = native.CStruct2[struct_point, point]
+  type enum_pointIndex = native.CUnsignedInt
   type point_s = native.Ptr[struct_point]
   type struct_bigStruct = native.CArray[Byte, native.Nat.Digit[native.Nat._1, native.Nat.Digit[native.Nat._1, native.Nat._2]]]
   type struct_structWithAnonymousStruct = native.CStruct2[native.CInt, native.CArray[Byte, native.Nat._8]]
-  def getPoint(): native.Ptr[struct_point] = native.extern
+  def setPoints(points: native.Ptr[struct_points], x1: native.CInt, y1: native.CInt, x2: native.CInt, y2: native.CInt): Unit = native.extern
+  def getPoint(points: native.Ptr[struct_points], pointIndex: enum_pointIndex): native.CInt = native.extern
+  def createPoint(): native.Ptr[struct_point] = native.extern
   def getBigStructSize(): native.CInt = native.extern
   def getCharFromAnonymousStruct(s: native.Ptr[struct_structWithAnonymousStruct]): native.CChar = native.extern
   def getIntFromAnonymousStruct(s: native.Ptr[struct_structWithAnonymousStruct]): native.CChar = native.extern
 }
 
 import Struct._
+
+object StructEnums {
+  final val enum_pointIndex_X1: enum_pointIndex = 0.toUInt
+  final val enum_pointIndex_Y1: enum_pointIndex = 1.toUInt
+  final val enum_pointIndex_X2: enum_pointIndex = 2.toUInt
+  final val enum_pointIndex_Y2: enum_pointIndex = 3.toUInt
+}
 
 object StructHelpers {
 
@@ -28,6 +40,15 @@ object StructHelpers {
   }
 
   def struct_point()(implicit z: native.Zone): native.Ptr[struct_point] = native.alloc[struct_point]
+
+  implicit class struct_points_ops(val p: native.Ptr[struct_points]) extends AnyVal {
+    def p1: native.Ptr[struct_point] = p._1
+    def p1_=(value: native.Ptr[struct_point]): Unit = !p._1 = !value
+    def p2: native.Ptr[point] = p._2
+    def p2_=(value: native.Ptr[point]): Unit = !p._2 = !value
+  }
+
+  def struct_points()(implicit z: native.Zone): native.Ptr[struct_points] = native.alloc[struct_points]
 
   implicit class struct_structWithAnonymousStruct_ops(val p: native.Ptr[struct_structWithAnonymousStruct]) extends AnyVal {
     def a: native.CInt = !p._1
