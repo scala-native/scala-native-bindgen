73cc31c817a2be4354fec72bba37516fae87ec68
kornilova-l <kornilova203@gmail.com>
2018-07-13 23:04:08 +0300

    Generate setters for fields of big structs

--- tests/samples/Struct.scala
+++ tests/samples/Struct.scala
@@ -52,28 +52,51 @@ object StructHelpers {
 
   implicit class struct_bigStruct_ops(val p: native.Ptr[struct_bigStruct]) extends AnyVal {
     def one: native.CLong = !p._1.cast[native.Ptr[native.CLong]]
+    def one_=(value: native.CLong): Unit = !p._1.cast[native.Ptr[native.CLong]] = value
     def two: native.CChar = !(p._1 + 8).cast[native.Ptr[native.CChar]]
+    def two_=(value: native.CChar): Unit = !(p._1 + 8).cast[native.Ptr[native.CChar]] = value
     def three: native.CInt = !(p._1 + 12).cast[native.Ptr[native.CInt]]
+    def three_=(value: native.CInt): Unit = !(p._1 + 12).cast[native.Ptr[native.CInt]] = value
     def four: native.CFloat = !(p._1 + 16).cast[native.Ptr[native.CFloat]]
+    def four_=(value: native.CFloat): Unit = !(p._1 + 16).cast[native.Ptr[native.CFloat]] = value
     def five: native.CDouble = !(p._1 + 24).cast[native.Ptr[native.CDouble]]
+    def five_=(value: native.CDouble): Unit = !(p._1 + 24).cast[native.Ptr[native.CDouble]] = value
     def six: native.Ptr[struct_point] = !(p._1 + 32).cast[native.Ptr[native.Ptr[struct_point]]]
+    def six_=(value: native.Ptr[struct_point]): Unit = !(p._1 + 32).cast[native.Ptr[native.Ptr[struct_point]]] = value
     def seven: native.CInt = !(p._1 + 40).cast[native.Ptr[native.CInt]]
+    def seven_=(value: native.CInt): Unit = !(p._1 + 40).cast[native.Ptr[native.CInt]] = value
     def eight: native.CInt = !(p._1 + 44).cast[native.Ptr[native.CInt]]
+    def eight_=(value: native.CInt): Unit = !(p._1 + 44).cast[native.Ptr[native.CInt]] = value
     def nine: native.CInt = !(p._1 + 48).cast[native.Ptr[native.CInt]]
+    def nine_=(value: native.CInt): Unit = !(p._1 + 48).cast[native.Ptr[native.CInt]] = value
     def ten: native.CInt = !(p._1 + 52).cast[native.Ptr[native.CInt]]
+    def ten_=(value: native.CInt): Unit = !(p._1 + 52).cast[native.Ptr[native.CInt]] = value
     def eleven: native.CInt = !(p._1 + 56).cast[native.Ptr[native.CInt]]
+    def eleven_=(value: native.CInt): Unit = !(p._1 + 56).cast[native.Ptr[native.CInt]] = value
     def twelve: native.CInt = !(p._1 + 60).cast[native.Ptr[native.CInt]]
+    def twelve_=(value: native.CInt): Unit = !(p._1 + 60).cast[native.Ptr[native.CInt]] = value
     def thirteen: native.CInt = !(p._1 + 64).cast[native.Ptr[native.CInt]]
+    def thirteen_=(value: native.CInt): Unit = !(p._1 + 64).cast[native.Ptr[native.CInt]] = value
     def fourteen: native.CInt = !(p._1 + 68).cast[native.Ptr[native.CInt]]
+    def fourteen_=(value: native.CInt): Unit = !(p._1 + 68).cast[native.Ptr[native.CInt]] = value
     def fifteen: native.CInt = !(p._1 + 72).cast[native.Ptr[native.CInt]]
+    def fifteen_=(value: native.CInt): Unit = !(p._1 + 72).cast[native.Ptr[native.CInt]] = value
     def sixteen: native.CInt = !(p._1 + 76).cast[native.Ptr[native.CInt]]
+    def sixteen_=(value: native.CInt): Unit = !(p._1 + 76).cast[native.Ptr[native.CInt]] = value
     def seventeen: native.CInt = !(p._1 + 80).cast[native.Ptr[native.CInt]]
+    def seventeen_=(value: native.CInt): Unit = !(p._1 + 80).cast[native.Ptr[native.CInt]] = value
     def eighteen: native.CInt = !(p._1 + 84).cast[native.Ptr[native.CInt]]
+    def eighteen_=(value: native.CInt): Unit = !(p._1 + 84).cast[native.Ptr[native.CInt]] = value
     def nineteen: native.CInt = !(p._1 + 88).cast[native.Ptr[native.CInt]]
+    def nineteen_=(value: native.CInt): Unit = !(p._1 + 88).cast[native.Ptr[native.CInt]] = value
     def twenty: native.CInt = !(p._1 + 92).cast[native.Ptr[native.CInt]]
+    def twenty_=(value: native.CInt): Unit = !(p._1 + 92).cast[native.Ptr[native.CInt]] = value
     def twentyOne: native.CInt = !(p._1 + 96).cast[native.Ptr[native.CInt]]
+    def twentyOne_=(value: native.CInt): Unit = !(p._1 + 96).cast[native.Ptr[native.CInt]] = value
     def twentyTwo: native.CInt = !(p._1 + 100).cast[native.Ptr[native.CInt]]
+    def twentyTwo_=(value: native.CInt): Unit = !(p._1 + 100).cast[native.Ptr[native.CInt]] = value
     def twentyThree: native.CInt = !(p._1 + 104).cast[native.Ptr[native.CInt]]
+    def twentyThree_=(value: native.CInt): Unit = !(p._1 + 104).cast[native.Ptr[native.CInt]] = value
   }
 
   def struct_bigStruct()(implicit z: native.Zone): native.Ptr[struct_bigStruct] = native.alloc[struct_bigStruct]
